0fd46541afadf5bf8dceb472408d9502416e1ab0
Daniel Trimble <Daniel.Trimble@jbhunt.com>
2017-05-11 12:03:02 -0500

    change to http

---
+++ app.js
@@ -0,0 +1,54 @@
+var restify = require('restify');
+var http = require('http');
+
+// var server = restify.createServer({
+//   name: 'MyApp'
+// });
+
+// server.use(restify.bodyParser({ mapParams: true }));
+
+//  server.post('/loadtest', function create(req, res, next) {
+
+//     restify.createJsonClient({url: 'http://jvdweb10901.jbhunt.com', requestTimeout: 50000, connectTimeout: 10000})
+//             .post('/ws_rateService/service/currentMarketRatesForExternal', req.body, function(err, request, response, obj) {
+//         if(err){
+//             console.log(err);
+            
+//             session.send("No rate found");
+//             session.replaceDialog("/");
+//         }
+
+//         if(response.body){ 
+
+//             console.log(response.body);
+//             res.body = response.body;
+            
+//             res.send(200);
+//             return next();
+//         } else {
+//             res.body = 'fail';
+//             res.send(500);
+//             return next();
+//         }
+
+            
+//     });
+        
+//  });
+
+ 
+// server.listen(process.env.PORT || 8080);
+
+
+
+var server = http.createServer(function(request, response) {
+
+response.writeHead(200, {"Content-Type": "text/plain"});
+response.end("Hello Azure!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port); 
